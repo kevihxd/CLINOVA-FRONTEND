e9a5d6dc025811dd819b7af3222ba4a1aa62a4d8
kevihxd <kevihxd@gmail.com>
2026-04-14 17:12:31 -0500

    feat arreglo v2

--- src/services/httpClient.ts
+++ src/services/httpClient.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const http = axios.create({ 
-    baseURL: "https://realtors-furniture-cars-reservoir.trycloudflare.com",
+    baseURL: "https://portfolio-kelkoo-mae-presented.trycloudflare.com",
     timeout: 10000,
     headers: {
         "Content-Type": "application/json",
